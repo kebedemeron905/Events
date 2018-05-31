d329df183651783b509e972794a743a5f14bb16c
Meron Kebede <kebedemeron905@gmail.com>
2018-05-31 10:17:32 -0400

    Default url debugging

--- bachata_django/bachata_django/settings.py
+++ bachata_django/bachata_django/settings.py
@@ -26,7 +26,7 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'qj%^m%&n$_0ehkc(q6y_z18zl2qhbk*rk)v)g1v@!f6p3qky-%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
